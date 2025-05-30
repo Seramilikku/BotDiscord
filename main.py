bf3942fe0e78762af1ebe1ed80f7b03b74ee378b
Ezi <fakenrono@gmail.com>
2025-05-30 15:18:05 +0700

    pertanyaan random

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 import os, discord
 from dotenv import load_dotenv
 from discord.ext import commands
+import random
 
 load_dotenv()
 
-client = commands.Bot(command_prefix='=', intents=discord.Intents.all())
+client = commands.Bot(command_prefix='.', intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
     print("Succes: Yuira is connected to Discord")
-    
+
+#Test Bot    
 @client.command()
-async def ping(ctx):
+async def nya(ctx):
     await ctx.send("Nya!")
+
+#pertanyaan random bot    
+@client.command()
+async def yuira(ctx, *, question):
+    with open("responses.txt", "r") as f:
+        random_responses = f.readlines()
+        response = random.choice(random_responses)
+        
+    await ctx.send(response)
 
 client.run(os.getenv("TOKEN"))
